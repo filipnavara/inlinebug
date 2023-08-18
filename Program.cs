19a06e602f6dd2beaae8b46f33430baf4b17061d
Filip Navara <navara@emclient.com>
2023-08-18 13:40:55 +0200

    Repro

---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿var registrarHelper = Type.GetType("ObjCRuntime.RegistrarHelper, Microsoft.MacCatalyst");
+var lookupRegisteredTypeId = registrarHelper.GetMethod("LookupRegisteredTypeId", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic);
+Console.WriteLine(lookupRegisteredTypeId.Invoke(null, new object[] { typeof(Foundation.NSString) }));
